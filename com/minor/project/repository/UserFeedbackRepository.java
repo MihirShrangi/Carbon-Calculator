63f9583e19fe43fdfe8d1551e77fe72d5fb07f16
Mihir Shrangi <127875527+MihirShrangi@users.noreply.github.com>
2025-03-05 18:05:01 +0530

    Delete com/minor/project directory

--- com/minor/project/repository/UserFeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.minor.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
- 
-import com.minor.project.model.UserfeedbackDtls;
-
-public interface UserFeedbackRepository extends JpaRepository<UserfeedbackDtls, Integer>{
-
-}
--- com/minor/project/repository/UserPayRepository.java
+++
@@ -1,9 +0,0 @@
-package com.minor.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
- 
-import com.minor.project.model.UserpayDtls;
-
-public interface UserPayRepository extends JpaRepository<UserpayDtls, Integer>  {
-
-}
